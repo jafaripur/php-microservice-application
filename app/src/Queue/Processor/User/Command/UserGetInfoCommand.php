79be134b484799ef1354e458dbf1945af150677a
Araz <mjafaripur@yahoo.com>
2022-02-09 15:15:10 +0330

    Remove __construct method in processor file.

--- app/src/Queue/Processor/User/Command/UserGetInfoCommand.php
+++ app/src/Queue/Processor/User/Command/UserGetInfoCommand.php
@@ -5,17 +5,9 @@ declare(strict_types=1);
 namespace App\Queue\Processor\User\Command;
 
 use App\Queue\Processor\User\UserCommand;
-use Araz\MicroService\ProcessorConsumer;
-use Araz\MicroService\Queue;
-use Psr\Log\LoggerInterface;
 
 final class UserGetInfoCommand extends UserCommand
 {
-    public function __construct(Queue $queue, ProcessorConsumer $processorConsumer, LoggerInterface $logger)
-    {
-        parent::__construct($queue, $processorConsumer);
-    }
-
     public function execute(mixed $body): mixed
     {
         return [
